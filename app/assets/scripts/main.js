47a3d64ccf0067c230574151d9d9f07375b5bb4f
Nati Kaumusher <natan.kamuhser@gmail.com>
2019-03-02 17:45:26 +0200

    Complete the search section

--- app/assets/scripts/main.js
+++ app/assets/scripts/main.js
@@ -12,4 +12,30 @@ document.addEventListener('DOMContentLoaded', function() {
   };
   elems = document.querySelectorAll('.slider');
   instances = M.Slider.init(elems, options);
+
+  // Autocomplete
+  let destinations = {
+    "Aruba": null,
+    "Cancun Mexico": null,
+    "Hawaii": null,
+    "Florida": null,
+    "California": null,
+    "Jamaica": null,
+    "Europe": null,
+    "The Bahamas": null
+  }
+
+  // Sort function for sorting autocomplete results
+  let sortDestinations = function (a, b, inputString) {
+    return a.indexOf(inputString) - b.indexOf(inputString);
+  }
+
+  options = {
+    data: destinations,
+    sortFunctions: sortDestinations
+  };
+
+  elems = document.querySelectorAll('.autocomplete');
+  instances = M.Autocomplete.init(elems, options);
 })
+
