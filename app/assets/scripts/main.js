eeedb54abd1dd11b87fc4de5ebcde55c45f468fc
Nati Kaumusher <natan.kamuhser@gmail.com>
2019-03-02 15:09:45 +0200

    Completed the header section

--- app/assets/scripts/main.js
+++ app/assets/scripts/main.js
@@ -1,7 +1,15 @@
-/* Add sidenav event listener*/
+/* Init sidenav */
 document.addEventListener('DOMContentLoaded', function() {
   let elems=document.querySelectorAll('.sidenav');
   let options = {};
   let instances = M.Sidenav.init(elems, options);
-
+// Init slider
+  options = {
+    indicators: false,
+    height: 500,
+    transition: 500,
+    interval: 6000
+  };
+  elems = document.querySelectorAll('.slider');
+  instances = M.Slider.init(elems, options);
 })
