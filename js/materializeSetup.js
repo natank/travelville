95bc8441cadecfe83efd8317fc6e8883b187c34f
Nati Kaumusher <natan.kamuhser@gmail.com>
2019-04-15 21:57:18 +0300

    Started the search section

--- js/materializeSetup.js
+++ js/materializeSetup.js
@@ -4,4 +4,19 @@ document.addEventListener('DOMContentLoaded', function() {
 
   elems = document.querySelectorAll('.slider');
   var instances = M.Slider.init(elems);
+
+  options = {
+    data: {
+      Aruba: null,
+      'Cancun Mexico': null,
+      Hawaii: null,
+      Florida: null,
+      California: null,
+      Jamaica: null,
+      Europe: null,
+      'The Bahamas': null
+    }
+  };
+  elems = document.querySelectorAll('.autocomplete');
+  var instances = M.Autocomplete.init(elems, options);
 });
